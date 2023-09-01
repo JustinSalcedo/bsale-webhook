ba798e69784b2745019d798cef404a6637e19c8e
Justin Salcedo <nitsuj162114@gmail.com>
2023-09-01 07:44:12 -0700

    PDF Printer: Linux unsupported. Testing on Windows

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,26 +1,26 @@
 import { Express } from 'express'
 import expressLoader from './express'
 import LoggerInstance from './logger'
-import ptp from 'pdf-to-printer'
+import { getDefaultPrinter, getPrinters } from 'pdf-to-printer'
 
 export default function ({ expressApp }: { expressApp: Express }) {
     // Check initial printer connection
-    // ptp.getDefaultPrinter().then(defaultPrinter => {
-    //     if (defaultPrinter) {
-    //         const { deviceId, name, paperSizes } = defaultPrinter
-    //         LoggerInstance.info(`
-    //             Default printer
-    //             \n- Device ID: ${deviceId}
-    //             \n- Name: ${name}
-    //             \n- Paper sizes: ${paperSizes}
-    //         `)
-    //     }
-    //     else LoggerInstance.warn('No default printer found')
-    // })
-    // ptp.getPrinters().then(printers => {
-    //     if (printers) LoggerInstance.info(printers)
-    // })
-    LoggerInstance.debug(ptp)
+    getDefaultPrinter().then(defaultPrinter => {
+        if (defaultPrinter) {
+            const { deviceId, name, paperSizes } = defaultPrinter
+            LoggerInstance.info(`
+                Default printer
+                \n- Device ID: ${deviceId}
+                \n- Name: ${name}
+                \n- Paper sizes: ${paperSizes}
+            `)
+        }
+        else LoggerInstance.warn('No default printer found')
+    }).catch(e => LoggerInstance.error(e))
+    getPrinters().then(printers => {
+        if (printers) LoggerInstance.info(printers)
+    }).catch(e => LoggerInstance.error(e))
+    // LoggerInstance.debug(ptp)
 
     // Load express routes
     expressLoader({ app: expressApp })
