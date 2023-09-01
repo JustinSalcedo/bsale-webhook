5906604eeda0334981dd9506a8d762c8e2ca6c0c
Justin Salcedo <nitsuj162114@gmail.com>
2023-09-01 07:48:41 -0700

    PDF Printer: log information for each printer

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,26 +1,25 @@
 import { Express } from 'express'
 import expressLoader from './express'
 import LoggerInstance from './logger'
-import { getDefaultPrinter, getPrinters } from 'pdf-to-printer'
+import { Printer, getDefaultPrinter, getPrinters } from 'pdf-to-printer'
 
 export default function ({ expressApp }: { expressApp: Express }) {
     // Check initial printer connection
+    const logPrinterInfo = ({ deviceId, name, paperSizes }: Printer) => LoggerInstance.info(`
+        Default printer
+        \n- Device ID: ${deviceId}
+        \n- Name: ${name}
+        \n- Paper sizes: ${paperSizes}
+    `)
+
     getDefaultPrinter().then(defaultPrinter => {
-        if (defaultPrinter) {
-            const { deviceId, name, paperSizes } = defaultPrinter
-            LoggerInstance.info(`
-                Default printer
-                \n- Device ID: ${deviceId}
-                \n- Name: ${name}
-                \n- Paper sizes: ${paperSizes}
-            `)
-        }
+        if (defaultPrinter) logPrinterInfo(defaultPrinter)
         else LoggerInstance.warn('No default printer found')
     }).catch(e => LoggerInstance.error(e))
+
     getPrinters().then(printers => {
-        if (printers) LoggerInstance.info(printers)
+        if (printers) printers.map(logPrinterInfo)
     }).catch(e => LoggerInstance.error(e))
-    // LoggerInstance.debug(ptp)
 
     // Load express routes
     expressLoader({ app: expressApp })
